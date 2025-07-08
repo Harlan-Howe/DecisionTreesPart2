02a85c3fb39387edfdce70b867c04825ed98c00a
Harlan Howe <harlan.howe@kinkaid.org>
2025-07-08 17:11:41 -0500

    Improved documentation.

--- ConditionFile.py
+++ ConditionFile.py
@@ -23,10 +23,21 @@ class NumericCondition(GenericCondition):
     by this Condition greater than the threshold held by this condition?
     """
     def __init__(self, attribute_name: str, threshold: float):
+        """
+        sets this NumericCondition object up with the given attribute name and threshold.
+        :param attribute_name: the name of the attribute we'll be checking
+        :param threshold: the numerical value that will determine if the value stored in the attribute is big enough.
+        """
         super(NumericCondition, self).__init__(attribute_name)
         self.threshold_value = threshold
 
     def ask(self, answer_group: AnswerGroup[float]) -> bool:
+        """
+        asks the given AnswerGroup has a value for this condition's attribute that is larger than this condition's
+        threshold.
+        :param answer_group: the AnswerGroup in question
+        :return: whether the AnswerGroup instance has a large enough value
+        """
         return answer_group.get_attribute_for_name(self.attribute_name) > self.threshold_value
 
     def __repr__(self):
